511bd6acad4a751e07f5b39d6ea3f11016a6d203
Eric <doggoantinuke@gmail.com>
2024-11-01 15:26:39 -0500

    Added api key, and bot run code added a (broken) ping command

--- __init__.py
+++ __init__.py
@@ -1,3 +1,30 @@
 import hikari
-import dotenv
-print('Hello World!')
+from dotenv import load_dotenv
+import lightbulb
+import os
+
+#Key pathing
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(BASEDIR, '.env'))
+Key = os.getenv("API_KEY")
+#Hikari
+bot = hikari.GatewayBot(Key)
+
+#Lightbulb
+client = lightbulb.client_from_app(bot)
+bot.subscribe(hikari.StartingEvent, client.start)
+
+#Ping Command
+@client.register()
+class Ping(
+    lightbulb.SlashCommand,
+    name = "pingvalid",
+    description="sigma",
+):
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        await ctx.repsond("PONG")
+
+
+
+bot.run()
