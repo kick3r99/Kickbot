8c968777131d13b3f3426e402828692904d02663
Eric <doggoantinuke@gmail.com>
2024-11-01 16:15:39 -0500

    Renamed ping.py to test-commands.py added ping command to test-commands.py

--- bot/extensions/test-commands.py
+++ bot/extensions/test-commands.py
@@ -10,3 +10,10 @@ class Echo(lightbulb.SlashCommand, name="echo", description="Repeats the given t
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
         await ctx.respond(self.text)
+
+@loader.command
+class Ping(lightbulb.SlashCommand, name="ping", description="Checks the bot is alive"):
+    
+    @lightbulb.invoke
+    async def invoke(self, ctx: lightbulb.Context) -> None:
+        await ctx.respond("Pong!")
