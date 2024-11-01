89b109c1953c2c1f64fbfd53d8bc6ba6f737f4ae
Eric <doggoantinuke@gmail.com>
2024-11-01 16:11:59 -0500

    added echo command to ping.py

--- bot/extensions/ping.py
+++ bot/extensions/ping.py
@@ -3,9 +3,10 @@ import lightbulb
 loader = lightbulb.Loader()
 
 
+@loader.command
+class Echo(lightbulb.SlashCommand, name="echo", description="Repeats the given text"):
+    text: str = lightbulb.string("text", "The text to repeat")
 
-
-class Ping(lightbulb.SlashCommand, name="ping", description="Checks the bot is alive"):
     @lightbulb.invoke
     async def invoke(self, ctx: lightbulb.Context) -> None:
-        await ctx.respond("Pong!")
+        await ctx.respond(self.text)
