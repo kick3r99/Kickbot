dc14d81ec1245fdc52b325a554d44270f4e5e20f
Eric <doggoantinuke@gmail.com>
2024-11-21 11:23:49 -0600

    Further blueskypost changes

--- bot/extensions/bs.py
+++ bot/extensions/bs.py
@@ -1,10 +1,13 @@
 import os
-from profanity_check import predict
+
+import hikari
 import lightbulb
 from atproto import Client
 from dotenv import load_dotenv
-import hikari
-import  bot
+from profanity_check import predict
+
+import bot
+
 # atproto is bsky api fyi
 # getting pass and user from .env
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -19,6 +22,11 @@ bsclient.login(User, Pass)
 
 loader = lightbulb.Loader()
 
+# use these in place of username and avatar calls
+# allows it to be universal and work with any login
+handle = bsclient.me.handle
+avatar = bsclient.me.avatar
+
 
 @loader.command
 class Bsky(lightbulb.SlashCommand, name="blueskypost", description="posts to bsky"):
@@ -31,20 +39,19 @@ class Bsky(lightbulb.SlashCommand, name="blueskypost", description="posts to bsk
             await ctx.respond("Please avoid Profanity")
         else:
             post = bsclient.send_post(self.text)
-            link = f'https://bsky.app/profile/botiusmaximus.bsky.social/post/{post.uri.split('/')[-1]}'
+            link = f'https://bsky.app/profile/{handle}/post/{post.uri.split('/')[-1]}'
             emb = (hikari.Embed(
-            timestamp=bot.time(),
-            url = link,
-            title=f"Your post has been posted.")
+                timestamp=bot.time(),
+                url=link,
+                title=f"Your post has been posted.")
             .set_author(
-            name="@botiusmaximus.bsky.social",
-            url="https://bsky.app/profile/botiusmaximus.bsky.social",
-            icon="https://cdn.bsky.app/img/avatar/plain/did:plc:ut64q4bmsk2q2zi4kfkxwdec/bafkreiavv25wovn2e6qol3nyazpraeubsulo6vr5i2w2jgvu5ii6tpvkji@jpeg"
+                name=handle,
+                url=f"https://bsky.app/profile/{handle}",
+                icon=avatar
             )
             .set_footer(
-            text=f"Requested by {ctx.member.display_name}",
-            icon=ctx.member.avatar_url,
-                )
+                text=f"Requested by {ctx.member.display_name}",
+                icon=ctx.member.avatar_url,
+            )
             )
             await ctx.respond(emb)
-
